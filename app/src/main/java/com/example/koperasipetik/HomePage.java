fee10fcb99c7e7c798ec145322cbad9b45a5a2e5
tubagus sulaeman <sidikibnu301@gmail.com>
2019-04-11 10:51:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/koperasipetik/HomePage.java
@@ -0,0 +1,13 @@
+package com.example.koperasipetik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_homepage);
+    }
+}
